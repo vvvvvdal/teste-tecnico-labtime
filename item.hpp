f4a1b68b7ce5ab4b982c5f7b5ee3b87b0794f0f1
felipe vidal <136502385+vvvvvdal@users.noreply.github.com>
2026-04-13 17:11:40 -0300

    update

--- item.hpp
+++ item.hpp
@@ -1,19 +1,40 @@
 #pragma once
 
+#ifndef ITEM_HPP
+#define ITEM_HPP
+
 #define EMPILHAVEL true
 #define NAO_EMPILHAVEL false
-#define TAM_UNICO 1
-#define TAM_MAX 64
+#define TAM_MAX_UNICO 1
+#define TAM_MAX_PILHA 64
+#define TAM_NULO 0
+#define QTD_ITENS 6
+#define QTD_NULA 0
 
 #include <iostream>
+#include <array>
 using namespace std;
 
 class Item {
 private:
     string nome;
     bool empilhavel;
+    int qtd;
     int tam_max;
 public:
-    Item(string nome, bool empilhavel, int tam_max);
-    void inicializar_itens();
+    Item();
+    Item(string nome, bool empilhavel, int qtd, int tam_max);
+    string get_nome();
+    bool get_empilhavel();
+    int get_qtd();
+
+    void set_qtd(int quantidade);
 };
+
+class Itens{
+public:
+    array<Item,6> itens;
+    Itens();
+};
+
+#endif
