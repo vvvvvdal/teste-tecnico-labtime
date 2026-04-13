13b89e8ba4b3ac4d3e40273c6e57c9fa30811b75
felipe vidal <136502385+vvvvvdal@users.noreply.github.com>
2026-04-13 17:12:10 -0300

    update

--- main.cpp
+++ main.cpp
@@ -1,8 +1,13 @@
 #include <iostream>
 #include "inventario.hpp"
+#include "item.hpp"
 using namespace std;
 
 int main() {
+    Inventario inv;
+    inv.adicionar_item(1, "espada");
+    inv.adicionar_item(3, "flecha");
+    inv.print_inventario();
 
     return 0;
 }
