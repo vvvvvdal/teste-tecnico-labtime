f8c38189069ef3c7a110e2e452dc6e575ebc3222
felipe vidal <136502385+vvvvvdal@users.noreply.github.com>
2026-04-13 17:11:28 -0300

    update

--- item.cpp
+++ item.cpp
@@ -1,12 +1,30 @@
 #include "item.hpp"
 
-Item::Item(string nome, bool empilhavel, int tam_max): nome(nome), empilhavel(empilhavel), tam_max(tam_max) {}
+Item::Item(): nome("Vazio"), empilhavel(NAO_EMPILHAVEL), qtd(QTD_NULA), tam_max(TAM_NULO) {}
 
-void Item::inicializar_itens() {
-    Item espada("Espada", NAO_EMPILHAVEL, TAM_UNICO);
-    Item escudo("Escudo", NAO_EMPILHAVEL, TAM_UNICO);
-    Item arco("Arco", NAO_EMPILHAVEL, TAM_UNICO);
-    Item flecha("Flecha", EMPILHAVEL, TAM_MAX);
-    Item pocao("Poção", EMPILHAVEL, TAM_MAX);
-    Item tocha("Tocha", EMPILHAVEL, TAM_MAX);
+Item::Item(string nome, bool empilhavel, int qtd, int tam_max): nome(nome), empilhavel(empilhavel), qtd(QTD_NULA), tam_max(tam_max) {}
+
+string Item::get_nome() {
+    return nome;
+}
+
+bool Item::get_empilhavel() {
+    return empilhavel;
+}
+
+int Item::get_qtd() {
+    return qtd;
+}
+
+void Item::set_qtd(int quantidade) {
+    qtd = quantidade;
+}
+
+Itens::Itens() {
+    itens[0] = Item("Espada", NAO_EMPILHAVEL, QTD_NULA, TAM_MAX_UNICO);
+    itens[1] = Item("Escudo", NAO_EMPILHAVEL, QTD_NULA, TAM_MAX_UNICO);
+    itens[2] = Item("Arco", NAO_EMPILHAVEL, QTD_NULA, TAM_MAX_UNICO);
+    itens[3] = Item("Flecha", EMPILHAVEL, QTD_NULA, TAM_MAX_PILHA);
+    itens[4] = Item("Poção", EMPILHAVEL, QTD_NULA, TAM_MAX_PILHA);
+    itens[5] = Item("Tocha", EMPILHAVEL, QTD_NULA, TAM_MAX_PILHA);
 }
