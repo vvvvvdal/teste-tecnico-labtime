3b6de73cd440a0f7d8035ba75d69709e28403b07
felipe vidal <136502385+vvvvvdal@users.noreply.github.com>
2026-04-15 10:57:57 -0300

    update (1.1)

--- auxiliares.hpp
+++ auxiliares.hpp
@@ -3,12 +3,19 @@
 #ifndef AUXILIARES_HPP
 #define AUXILIARES_HPP
 
+#define TAM_MAX_NOME 128
+#define TAM_MIN_INVENTARIO 6
+#define TAM_MAX_INVENTARIO 1000
+#define TAM_MAX_DIGITOS 9
+
 #include <iostream>
 #include <limits>
 using namespace std;
 
 void limpar_buffer_entrada();
-int get_inteiro();
-int get_tam_max_inventario();
+int ler_inteiro();
+int ler_tam_max_inventario();
+void formatar_nome(string &nome);
+string ler_nome();
 
 #endif
